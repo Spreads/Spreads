857b4bb25a9e556f86374472511e70672bf9cabb
Buybackoff <vbaybekov@gmail.com>
2015-02-17 17:34:01 +0400

    Bootstrapper

---
+++ src/Bootstrapper/ABI.cs
@@ -0,0 +1,241 @@
+/*
+ *                      Yeppp! library implementation
+ *
+ * This file is part of Yeppp! library and licensed under the New BSD license.
+ * See LICENSE.txt for the full text of the license.
+ */
+
+namespace Bootstrapper
+{
+	/// <summary>Application binary interface.</summary>
+	/// <seealso cref="Library.GetProcessABI" />
+	public struct ABI
+	{
+
+		/// <summary>Application binary interface not known to the library.</summary>
+		/// <remarks>This value is never returned on supported platforms.</remarks>
+		public static readonly ABI Unknown        = new ABI(0);
+		/// <summary>Windows x86 ABI.</summary>
+		public static readonly ABI Windows_X86    = new ABI(10);
+		/// <summary>Windows x86-64 ABI.</summary>
+		public static readonly ABI Windows_X86_64 = new ABI(11);
+		/// <summary>Windows IA64 ABI.</summary>
+		public static readonly ABI Windows_IA64   = new ABI(12);
+		/// <summary>Windows ARM ABI.</summary>
+		public static readonly ABI Windows_ARM    = new ABI(13);
+		/// <summary>OS X x86 ABI.</summary>
+		public static readonly ABI OSX_X86     = new ABI(20);
+		/// <summary>OS X x86-64 ABI.</summary>
+		public static readonly ABI OSX_X86_64  = new ABI(21);
+		/// <summary>OS X PowerPC ABI.</summary>
+		public static readonly ABI OSX_PPC     = new ABI(22);
+		/// <summary>OS X PowerPC 64 ABI.</summary>
+		public static readonly ABI OSX_PPC64   = new ABI(23);
+		/// <summary>Linux x86 ABI.</summary>
+		public static readonly ABI Linux_X86      = new ABI(30);
+		/// <summary>Linux x86-64 ABI.</summary>
+		public static readonly ABI Linux_X86_64   = new ABI(31);
+		/// <summary>Linux x32 ABI.</summary>
+		public static readonly ABI Linux_X32      = new ABI(32);
+		/// <summary>Linux IA64 ABI.</summary>
+		public static readonly ABI Linux_IA64     = new ABI(33);
+		/// <summary>Linux K1OM (Xeon Phi) ABI.</summary>
+		public static readonly ABI Linux_K1OM     = new ABI(34);
+		/// <summary>Linux Legacy ARM ABI (OABI).</summary>
+		public static readonly ABI Linux_ARM      = new ABI(40);
+		/// <summary>Linux ARM EABI with soft-float calling convention (armel, gnueabi).</summary>
+		public static readonly ABI Linux_ARMEL    = new ABI(41);
+		/// <summary>Linux ARM EABI with hard-float calling convention (armhf, gnueabihf).</summary>
+		public static readonly ABI Linux_ARMHF    = new ABI(42);
+		/// <summary>Linux ARM64 (AArch64) ABI.</summary>
+		public static readonly ABI Linux_ARM64    = new ABI(43);
+		/// <summary>Linux PowerPC ABI.</summary>
+		public static readonly ABI Linux_PPC      = new ABI(50);
+		/// <summary>Linux PowerPC 64 ABI.</summary>
+		public static readonly ABI Linux_PPC64    = new ABI(51);
+
+		private readonly uint id;
+
+		internal ABI(uint id)
+		{
+			this.id = id;
+		}
+
+		internal uint Id
+		{
+			get
+			{
+				return this.id;
+			}
+		}
+
+		/// <summary>Compares for equality with another <see cref="ABI" /> object.</summary>
+		/// <remarks>Comparison is performed by value.</remarks>
+		public bool Equals(ABI other)
+		{
+			return this.id == other.id;
+		}
+
+		/// <summary>Compares for equality with another object.</summary>
+		/// <remarks>Comparison is performed by value.</remarks>
+		public override bool Equals(System.Object other)
+		{
+			if (other == null || GetType() != other.GetType())
+				return false;
+
+			return this.Equals((ABI)other);
+		}
+
+		/// <summary>Provides a hash for the object.</summary>
+		/// <remarks>Non-equal <see cref="ABI" /> objects are guaranteed to have different hashes.</remarks>
+		public override int GetHashCode()
+		{
+			return unchecked((int)this.id);
+		}
+
+		/// <summary>Provides a string ID for the object.</summary>
+		/// <remarks>The string ID starts with a Latin letter and contains only Latin letters, digits, and underscore symbol.</remarks>
+		/// <seealso cref="Description" />
+		public override string ToString()
+		{
+			switch (this.id)
+			{
+				case 10:
+					return "Windows_X86";
+				case 11:
+					return "Windows_X86_64";
+				case 12:
+					return "Windows_IA64";
+				case 13:
+					return "Windows_ARM";
+				case 20:
+					return "OSX_X86";
+				case 21:
+					return "OSX_X86_64";
+				case 22:
+					return "OSX_PPC";
+				case 23:
+					return "OSX_PPC64";
+				case 30:
+					return "Linux_X86";
+				case 31:
+					return "Linux_X86_64";
+				case 32:
+					return "Linux_X32";
+				case 33:
+					return "Linux_IA64";
+				case 34:
+					return "Linux_K1OM";
+				case 40:
+					return "Linux_ARM";
+				case 41:
+					return "Linux_ARMEL";
+				case 42:
+					return "Linux_ARMHF";
+				case 43:
+					return "Linux_ARM64";
+				case 50:
+					return "Linux_PPC";
+				case 51:
+					return "Linux_PPC64";
+				default:
+					return "Unknown";
+			}
+		}
+
+		/// <summary>Checks if the object represents one of Windows-specific ABIs.</summary>
+		/// <returns>true if this is a Windows ABI and false otherwise.</returns>
+		/// <seealso cref="ABI.IsUnix" />
+		/// <seealso cref="ABI.IsLinux" />
+		/// <seealso cref="ABI.IsOSX" />
+		public bool IsWindows()
+		{
+			return ((this.id >= 10) && (this.id < 20));
+		}
+
+		/// <summary>Checks if the object represents one of Unix-specific ABIs.</summary>
+		/// <returns>true if this is a Unix ABI and false otherwise.</returns>
+		/// <seealso cref="ABI.IsOSX" />
+		/// <seealso cref="ABI.IsLinux" />
+		/// <seealso cref="ABI.IsWindows" />
+		public bool IsUnix()
+		{
+			return this.id >= 20;
+		}
+
+		/// <summary>Checks if the object represents one of Linux-specific ABIs.</summary>
+		/// <returns>true if this is a Linux ABI and false otherwise.</returns>
+		/// <seealso cref="ABI.IsUnix" />
+		/// <seealso cref="ABI.IsOSX" />
+		/// <seealso cref="ABI.IsWindows" />
+		public bool IsLinux()
+		{
+			return this.id >= 30;
+		}
+
+		/// <summary>Checks if the object represents one of OSX-specific ABIs.</summary>
+		/// <returns>true if this is an OSX ABI and false otherwise.</returns>
+		/// <seealso cref="ABI.IsUnix" />
+		/// <seealso cref="ABI.IsLinux" />
+		/// <seealso cref="ABI.IsWindows" />
+		public bool IsOSX()
+		{
+			return ((this.id >= 20) && (this.id < 30));
+		}
+
+		/// <summary>Provides a description for the object.</summary>
+		/// <remarks>The description can contain spaces and non-ASCII characters.</remarks>
+		/// <seealso cref="ToString()" />
+		public string Description
+		{
+			get
+			{
+				switch (this.id)
+				{
+					case 10:
+						return "Windows x86 ABI";
+					case 11:
+						return "Windows x86-64 ABI";
+					case 12:
+						return "Windows IA64 ABI";
+					case 13:
+						return "Windows ARM ABI";
+					case 20:
+						return "OS X x86 ABI";
+					case 21:
+						return "OS X x86-64 ABI";
+					case 22:
+						return "OS X PowerPC ABI";
+					case 23:
+						return "OS X PowerPC 64 ABI";
+					case 30:
+						return "Linux x86 ABI";
+					case 31:
+						return "Linux x86-64 ABI";
+					case 32:
+						return "Linux x32 ABI";
+					case 33:
+						return "Linux IA64 ABI";
+					case 34:
+						return "Linux K1OM (Xeon Phi) ABI";
+					case 40:
+						return "Linux Legacy ARM ABI (OABI)";
+					case 41:
+						return "Linux ARM EABI with soft-float calling convention (armel, gnueabi)";
+					case 42:
+						return "Linux ARM EABI with hard-float calling convention (armhf, gnueabihf)";
+					case 43:
+						return "Linux ARM64 (AArch64) ABI";
+					case 50:
+						return "Linux PowerPC ABI";
+					case 51:
+						return "Linux PowerPC 64 ABI";
+					default:
+						return "Unknown ABI";
+				}
+			}
+		}
+
+	}
+
+}
---
+++ src/Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Bootstrapper {
+
+    public static class Bootstrapper {
+
+        public static void Main() {
+            //Loader.CompressFolder(@"C:\Users\Sun\MD\CS\Public Projects\Spreads\lib\msvcrt\x32");
+            //Loader.ExtractFolder(@"C:\Users\Sun\MD\CS\Public Projects\Spreads\lib\msvcrt\x64.zip", @"C:\Users\Sun\MD\CS\Public Projects\Spreads\lib\msvcrt");
+
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new[] { 1, 2, 3, 4, 5 });
+            Console.WriteLine("Running Bootstrapper: " + json);
+            //YepppTest.Run();
+            Console.ReadLine();
+        }
+
+        // Bootstrapper extracts dlls embedded as resource to the app folder.
+        // Those dlls could contain other embedded dlls, which are extracted recursively as Russian dolls.
+        // Bootstrapper overwrites existing files 
+        // TODO major version
+
+        // AppData/fi.im/bin/ - all dlls and exes
+        // AppData/fi.im/config/config.sdb - all app and users configuration
+        // LocalAppData/fi.im/data - data folder
+        //                   /data/local.sdb - data for not logged-in user
+        //                   /data/userid.sdb - data for not logged-in user
+        // Documents/Modules - distributes LGPL libraries + user libraries. 
+        //                      All are loaded to app domain
+
+
+        // Botstrap self
+        static Bootstrapper() {
+
+            Bootstrapper.Bootstrap<Loader>(
+                new[] { "yeppp" },
+                new[] { "Newtonsoft.Json.dll" },
+                null,
+                null,
+                () => {
+                    if (!Directory.Exists(AppFolder)) {
+                        Directory.CreateDirectory(AppFolder);
+                    }
+
+                    if (!Directory.Exists(Path.Combine(AppFolder, "x32"))) {
+                        Directory.CreateDirectory(Path.Combine(AppFolder, "x32"));
+                    }
+
+                    if (!Directory.Exists(Path.Combine(AppFolder, "x64"))) {
+                        Directory.CreateDirectory(Path.Combine(AppFolder, "x64"));
+                    }
+
+                    if (!Directory.Exists(AppFolder)) {
+                        Directory.CreateDirectory(AppFolder);
+                    }
+
+                    if (!Directory.Exists(ConfigFolder)) {
+                        Directory.CreateDirectory(ConfigFolder);
+                    }
+
+                    if (!Directory.Exists(DataFolder)) {
+                        Directory.CreateDirectory(DataFolder);
+                    }
+
+                    if (!Directory.Exists(TempFolder)) {
+                        Directory.CreateDirectory(TempFolder);
+                    }
+                }, 
+                () => {
+                    //Yeppp.Library.Init();
+                },
+                () => {
+                    //Yeppp.Library.Release();
+                });
+        }
+
+        /// <summary>
+        /// in AppData and AppDataLocal
+        /// </summary>
+        private const string rootFolder = "fi.im";
+        private const string configSubFolder = "config";
+        private const string appSubFolder = "bin";
+        private const string dataSubFolder = "data";
+        private const string docFolder = "Docs";
+        private const string gplFolder = "Libraries";
+
+        internal static string ConfigFolder {
+            get {
+                return Path.Combine(
+              Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+              rootFolder, configSubFolder);
+            }
+        }
+
+        internal static string AppFolder {
+            get {
+                return Path.Combine(
+              Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+              rootFolder, appSubFolder);
+            }
+        }
+
+        internal static string DataFolder {
+            get {
+                return Path.Combine(
+              Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+              rootFolder, dataSubFolder);
+            }
+        }
+
+        private static string _tmpFolder = null;
+        internal static string TempFolder {
+            get {
+                if (_tmpFolder == null) {
+                    _tmpFolder = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                }
+                return _tmpFolder;
+            }
+        }
+
+        // keep references to avoid GC
+        internal static List<NativeLibrary> loadedLibraries = new List<NativeLibrary>();
+        private static Disposer disposer = new Disposer();
+        private static Action DisposeAction;
+        /// <summary>
+        /// From assembly with type T load libraries
+        /// </summary>
+        public static void Bootstrap<T>(string[] nativeLibNames = null,
+            string[] managedLibNames = null,
+            string[] resourceNames = null,
+            string[] serviceNames = null, // ensure these .exes are running
+            Action preCopyAction = null,
+            Action postCopyAction = null,
+            Action disposeAction = null) {
+
+            if (preCopyAction != null) preCopyAction.Invoke();
+
+            if (nativeLibNames != null) {
+                foreach (var nativeName in nativeLibNames) {
+                    loadedLibraries.Add(Loader.LoadNativeLibrary<T>(nativeName));
+                }
+            }
+
+            if (managedLibNames != null) {
+                foreach (var managedName in managedLibNames) {
+                    Trace.Assert(managedName.EndsWith(".dll"));
+                    Loader.ExtractResource<T>(managedName);
+                }
+            }
+            AppDomain.CurrentDomain.AssemblyResolve +=
+                new ResolveEventHandler(Loader.ResolveManagedAssembly);
+
+            if (resourceNames != null) {
+                foreach (var resourceName in resourceNames) {
+                    Loader.ExtractResource<T>(resourceName);
+                }
+            }
+
+            if (serviceNames != null) {
+                foreach (var serviceName in serviceNames) {
+                    Trace.Assert(serviceName.EndsWith(".exe"));
+                    // TODO run exe as singletone process by path
+                    throw new NotImplementedException("TODO start exe process");
+                }
+            }
+
+            if (postCopyAction != null) postCopyAction.Invoke();
+
+            DisposeAction = disposeAction;
+        }
+
+
+        private class Disposer : IDisposable {
+            public void Dispose() {
+                if(Bootstrapper.DisposeAction != null) Bootstrapper.DisposeAction.Invoke();
+                foreach (var loadedLibrary in Bootstrapper.loadedLibraries) {
+                    if (loadedLibrary != null) loadedLibrary.Dispose();
+                }
+                try {
+                    Directory.Delete(Bootstrapper.TempFolder, true);
+                } catch {
+                }
+            }
+
+            ~Disposer() {
+                Dispose();
+            }
+        }
+    }
+}
