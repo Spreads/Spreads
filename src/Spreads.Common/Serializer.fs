499732ca7b833d0acdb5a2db0ba1e1181f33067a
buybackoff <vbaybekov@gmail.com>
2015-03-23 19:13:11 +0300

    add missing method to Serializer

--- src/Spreads.Common/Serializer.fs
+++ src/Spreads.Common/Serializer.fs
@@ -2,5 +2,6 @@
 
 type ISerializer =
     abstract Serialize: 'T -> byte[]
+    abstract Serialize: obj -> byte[]
     abstract Deserialize: byte[] -> 'T
     abstract Deserialize: byte[] * System.Type -> obj
