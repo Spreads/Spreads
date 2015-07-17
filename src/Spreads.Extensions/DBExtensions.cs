5a5afd0c5166a6835813f34ee253948eb52a6141
buybackoff <vbaybekov@gmail.com>
2015-07-17 23:26:42 +0300

    IPersistentOrderedMap

---
+++ src/Spreads.Extensions/DBExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spreads {
+
+    // TODO move to SpreadsDB project
+    public interface IPersistentOrderedMap<K,V> : IOrderedMap<K, V>
+    {
+        // TODO WriteAck with get, set (or only get, this depends on implementation)
+
+        /// <summary>
+        /// Persist all unsaved changes 
+        /// </summary>
+        void Flush();
+    }
+}
