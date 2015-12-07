ba24c28a63895a68f330362f25d33a77c9553de3
buybackoff <vbaybekov@gmail.com>
2015-12-07 04:23:03 +0300

    Add Enumerable for reference

---
+++ src/Spreads.Core/Error.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Linq;
+namespace Spreads.System.Linq {
+    internal static class Error {
+        internal static Exception ArgumentNull(string s) { return new ArgumentNullException(s); }
+
+        internal static Exception ArgumentOutOfRange(string s) { return new ArgumentOutOfRangeException(s); }
+
+        internal static Exception MoreThanOneElement() { return new InvalidOperationException("SR.MoreThanOneElement"); }
+
+        internal static Exception MoreThanOneMatch() { return new InvalidOperationException("SR.MoreThanOneMatch"); }
+
+        internal static Exception NoElements() { return new InvalidOperationException("SR.NoElements"); }
+
+        internal static Exception NoMatch() { return new InvalidOperationException("SR.NoMatch"); }
+
+        internal static Exception NotSupported() { return new NotSupportedException(); }
+    }
+}
