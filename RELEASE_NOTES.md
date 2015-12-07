706687209178cac9705c56a5b6b6327590edc00d
buybackoff <vbaybekov@gmail.com>
2015-12-07 12:43:32 +0300

    #0.1.7
    Forgot to merge with upstream in 0.1.6

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,4 +1,4 @@
-#### 0.1.6 - December 7, 2015
+#### 0.1.7 - December 7, 2015
 * Optimize Map() and arithmetics on series by 4x
 * Fix bug with explicit interface implementation in ProjectValuesWrapper
 
--- src/Spreads.Collections/AssemblyInfo.fs
+++ src/Spreads.Collections/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Spreads.Collections")>]
 [<assembly: AssemblyProductAttribute("Spreads")>]
 [<assembly: AssemblyDescriptionAttribute("Spreads")>]
-[<assembly: AssemblyVersionAttribute("0.1.6")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.6")>]
+[<assembly: AssemblyVersionAttribute("0.1.7")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.7")>]
 [<assembly: AssemblyCopyrightAttribute("(c) Victor Baybekov 2015")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.6"
+    let [<Literal>] Version = "0.1.7"
