b49a6cf6d3f4a57f085517e7a967bfd461bd8b5e
Victor Baybekov <vbaybekov@gmail.com>
2016-02-29 08:01:47 +0300

    WIP

--- tests/Spreads.Extensions.Tests/Storage/SQLite/SqlitePerformanceTest.cs
+++ tests/Spreads.Extensions.Tests/Storage/SQLite/SqlitePerformanceTest.cs
@@ -1,16 +1,13 @@
 using System;
-using System.Data;
-using System.IO;
-using System.Reflection;
 using System.Diagnostics;
 using Dapper;
-//using Dapper;
-using NUnit.Framework;
+using Microsoft.Data.Sqlite;
 using Microsoft.Data.Sqlite.Utilities;
-using static Microsoft.Data.Sqlite.TestUtilities.Constants;
+using NUnit.Framework;
+//using Dapper;
 
 
-namespace Microsoft.Data.Sqlite {
+namespace Spreads.Extensions.Tests.Storage.SQLite {
     [TestFixture]
     public class SqlitePerformanceTest {
         [SetUp]
@@ -20,30 +17,32 @@ namespace Microsoft.Data.Sqlite {
 
 
         [Test]
-        public void InsertSpeed() {
-            var connectionString = "Data Source=perf_test.db;";
+        public void InsertSpeed()
+        {
+            var connectionString =  "Data Source=perf_test.db;"; // "Data Source=:memory:";//
 
             using (var connection = new SqliteConnection(connectionString)) {
                 connection.Open();
-                connection.ExecuteNonQuery("PRAGMA main.page_size = 4096;");
+                //connection.ExecuteNonQuery("PRAGMA main.locking_mode=EXCLUSIVE;");
+                connection.ExecuteNonQuery("PRAGMA main.page_size = 4096; ");
                 connection.ExecuteNonQuery("PRAGMA main.cache_size = 10000;");
                 connection.ExecuteNonQuery("PRAGMA synchronous = OFF;"); // NORMAL or OFF 20% faster
                 connection.ExecuteNonQuery("PRAGMA journal_mode = WAL;");
                 connection.ExecuteNonQuery("PRAGMA main.cache_size = 5000;");
                 connection.ExecuteNonQuery("DROP TABLE IF EXISTS Numbers");
-                connection.ExecuteNonQuery("CREATE TABLE Numbers (Key INTEGER, Value REAL);");
+                connection.ExecuteNonQuery("CREATE TABLE Numbers (Key INTEGER, Value REAL, PRIMARY KEY(Key));");
 
                 var sw = new Stopwatch();
                 sw.Start();
-                for (int i = 0; i < 200000; i++) {
+                //var txn = connection.BeginTransaction();
+                for (int i = 0; i < 100000; i++) {
                     connection.ExecuteNonQuery($"INSERT INTO Numbers VALUES ({i}, {i});");
                     //connection.Execute("INSERT INTO Numbers VALUES (@Key, @Value);",
                     //    new[] { new { Key = (long)i, Value = (double)i } });
                 }
+                //txn.Commit();
                 sw.Stop();
                 Console.WriteLine($"Elapsed, msec {sw.ElapsedMilliseconds}");
-
-
 
             }
         }
--- tests/Spreads.Tests.CSProfile/Program.cs
+++ tests/Spreads.Tests.CSProfile/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using Spreads;
 using Spreads.Collections;
+using Spreads.Extensions.Tests.Storage.SQLite;
 
 
 public class MyInc {
@@ -15,7 +16,7 @@ namespace Spreads.Tests.CSProfile {
     class Program {
         static void Main(string[] args) {
             var bs = Bootstrap.Bootstrapper.Instance;
-            (new Microsoft.Data.Sqlite.SqlitePerformanceTest()).InsertSpeed();
+            (new SqlitePerformanceTest()).InsertSpeed();
             Console.ReadLine();
             Console.WriteLine(bs.AppFolder);
         }
