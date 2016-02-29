041a9a0d5ae9a7837320c87542da3bbabdf9352e
buybackoff <vbaybekov@gmail.com>
2016-02-29 11:58:34 +0300

    Add blob methods to SQLite

---
+++ src/Spreads.Extensions/Storage/SQLite/Interop/NativeMethodsEx.cs
@@ -0,0 +1,127 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Data.Sqlite.Interop {
+
+    internal static partial class NativeMethods {
+
+        public static int sqlite3_blob_bytes(Sqlite3BlobHandle pBlob)
+            => Sqlite3.blob_bytes(pBlob);
+
+        public static int sqlite3_blob_close(IntPtr pBlob)
+            => Sqlite3.blob_close(pBlob);
+
+        public static int sqlite3_blob_open(Sqlite3Handle ppDb, string db, string table, string column, long iRow,
+            int flags, out Sqlite3BlobHandle ppBlob) {
+            var zDb = MarshalEx.StringToHGlobalUTF8(db);
+            var zTable = MarshalEx.StringToHGlobalUTF8(table);
+            var zColumn = MarshalEx.StringToHGlobalUTF8(column);
+            try {
+                return Sqlite3.blob_open(ppDb, zDb, zTable, zColumn, iRow, flags, out ppBlob);
+            } finally {
+                if (zDb != IntPtr.Zero) {
+                    Marshal.FreeHGlobal(zDb);
+                }
+                if (zTable != IntPtr.Zero) {
+                    Marshal.FreeHGlobal(zTable);
+                }
+                if (zColumn != IntPtr.Zero) {
+                    Marshal.FreeHGlobal(zColumn);
+                }
+                
+            }
+        }
+
+        public static int sqlite3_blob_read(Sqlite3BlobHandle pBlob, IntPtr destination, int length, int offset)
+            => Sqlite3.blob_read(pBlob, destination, length, offset);
+
+        public static int sqlite3_blob_write(Sqlite3BlobHandle pBlob, IntPtr source, int length, int offset)
+            => Sqlite3.blob_write(pBlob, source, length, offset);
+
+
+        private partial interface ISqlite3 {
+            int blob_bytes(Sqlite3BlobHandle pBlob);
+
+            int blob_close(IntPtr pStmt);
+
+            int blob_open(Sqlite3Handle ppDb, IntPtr zDb, IntPtr zTable, IntPtr zColumn, long iRow, int flags,
+                out Sqlite3BlobHandle ppBlob);
+
+            int blob_read(Sqlite3BlobHandle pBlob, IntPtr destination, int length, int offset);
+
+            int blob_write(Sqlite3BlobHandle pBlob, IntPtr source, int length, int offset);
+        }
+
+        private partial class Sqlite3_sqlite3 : ISqlite3 {
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_bytes(Sqlite3BlobHandle pBlob);
+
+            public int blob_bytes(Sqlite3BlobHandle pBlob)
+                => sqlite3_blob_bytes(pBlob);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_close(IntPtr pBlob);
+
+            public int blob_close(IntPtr pBlob)
+                => sqlite3_blob_close(pBlob);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_open(Sqlite3Handle pDb, IntPtr zDb, IntPtr zTable, IntPtr zColumn, long iRow, int flags, out Sqlite3BlobHandle ppBlob);
+
+            public int blob_open(Sqlite3Handle ppDb, IntPtr zDb, IntPtr zTable, IntPtr zColumn, long iRow, int flags, out Sqlite3BlobHandle ppBlob)
+                => sqlite3_blob_open(ppDb, zDb, zTable, zColumn, iRow, flags, out ppBlob);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_read(Sqlite3BlobHandle pBlob, IntPtr destination, int length, int offset);
+
+            public int blob_read(Sqlite3BlobHandle pBlob, IntPtr destination, int length, int offset)
+                => sqlite3_blob_read(pBlob, destination, length, offset);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_write(Sqlite3BlobHandle pBlob, IntPtr source, int length, int offset);
+
+            public int blob_write(Sqlite3BlobHandle pBlob, IntPtr source, int length, int offset)
+                => sqlite3_blob_write(pBlob, source, length, offset);
+
+        }
+
+
+        private partial class Sqlite3_winsqlite3 : ISqlite3 {
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_bytes(Sqlite3BlobHandle pBlob);
+
+            public int blob_bytes(Sqlite3BlobHandle pBlob)
+                => sqlite3_blob_bytes(pBlob);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_close(IntPtr pBlob);
+
+            public int blob_close(IntPtr pBlob)
+                => sqlite3_blob_close(pBlob);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_open(Sqlite3Handle pDb, IntPtr zDb, IntPtr zTable, IntPtr zColumn, long iRow, int flags, out Sqlite3BlobHandle ppBlob);
+
+            public int blob_open(Sqlite3Handle ppDb, IntPtr zDb, IntPtr zTable, IntPtr zColumn, long iRow, int flags, out Sqlite3BlobHandle ppBlob)
+                => sqlite3_blob_open(ppDb, zDb, zTable, zColumn, iRow, flags, out ppBlob);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_read(Sqlite3BlobHandle pBlob, IntPtr destination, int length, int offset);
+
+            public int blob_read(Sqlite3BlobHandle pBlob, IntPtr destination, int length, int offset)
+                => sqlite3_blob_read(pBlob, destination, length, offset);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+            private static extern int sqlite3_blob_write(Sqlite3BlobHandle pBlob, IntPtr source, int length, int offset);
+
+            public int blob_write(Sqlite3BlobHandle pBlob, IntPtr source, int length, int offset)
+                => sqlite3_blob_read(pBlob, source, length, offset);
+
+        }
+    }
+
+}
+
---
+++ src/Spreads.Extensions/Storage/SQLite/Interop/Sqlite3BlobHandle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using static Microsoft.Data.Sqlite.Interop.Constants;
+
+namespace Microsoft.Data.Sqlite.Interop {
+
+    internal class Sqlite3BlobHandle : SafeHandle 
+    {
+        private Sqlite3BlobHandle() : base(IntPtr.Zero, ownsHandle: true) 
+        {
+        }
+
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        protected override bool ReleaseHandle() 
+        {
+            var rc = NativeMethods.sqlite3_blob_close(handle);
+            handle = IntPtr.Zero;
+            return rc == SQLITE_OK;
+        }
+    }
+}
+
