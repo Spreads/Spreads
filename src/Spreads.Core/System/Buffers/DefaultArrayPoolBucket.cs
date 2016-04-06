76451262a071bba060b4db59bb89fc35112a07b3
buybackoff <vbaybekov@gmail.com>
2016-04-06 19:52:45 +0300

    Add Buffers from corefx

---
+++ src/Spreads.Core/System/Buffers/DefaultArrayPoolBucket.cs
@@ -0,0 +1,118 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.Threading;
+
+namespace System.Buffers
+{
+    /// <summary>
+    /// Provides a thread-safe bucket containing buffers that can be Rented and Returned as part 
+    /// of a buffer pool; it should not be used independent of the pool.
+    /// </summary>
+    internal sealed class DefaultArrayPoolBucket<T>
+    {
+        private int _index;
+        private readonly T[][] _data;
+        internal readonly int _bufferLength;
+        private SpinLock _lock;
+        private bool _exhaustedEventSent;
+        private readonly int _poolId;
+
+        /// <summary>
+        /// Creates the pool with numberOfBuffers arrays where each buffer is of bufferLength length.
+        /// </summary>
+        internal DefaultArrayPoolBucket(int bufferLength, int numberOfBuffers, int poolId)
+        {
+            _lock = new SpinLock(Debugger.IsAttached); // only enable thread tracking if debugger is attached; it adds non-trivial overheads to Enter/Exit
+            _data = new T[numberOfBuffers][];
+            _bufferLength = bufferLength;
+            _exhaustedEventSent = false;
+            _poolId = poolId;
+        }
+
+        /// <summary>
+        /// Returns an array from the Bucket sized according to the Bucket size.
+        /// If the Bucket is empty, null is returned.
+        /// </summary>
+        /// <returns>Returns a valid buffer when the bucket has free buffers; otherwise, returns null</returns>
+        internal T[] Rent()
+        {
+            T[] buffer = null;
+
+            // Use a SpinLock since it is super lightweight
+            // and our lock is very short lived. Wrap in try-finally
+            // to protect against thread-aborts
+            bool taken = false;
+            try
+            {
+                _lock.Enter(ref taken);
+
+                // Check if all of our buffers have been rented
+                if (_index < _data.Length)
+                {
+                    buffer = _data[_index];
+                    if (buffer == null)
+                    {
+                        buffer = new T[_bufferLength];
+                        if (ArrayPoolEventSource.Log.IsEnabled())
+                            ArrayPoolEventSource.Log.BufferAllocated(
+                                Utilities.GetBufferId(buffer),
+                                _bufferLength,
+                                _poolId,
+                                Utilities.GetBucketId(this),
+                                ArrayPoolEventSource.BufferAllocationReason.Pooled);
+                    }
+                    _data[_index++] = null;
+                }
+                else if (_exhaustedEventSent == false)
+                {
+                    if (ArrayPoolEventSource.Log.IsEnabled())
+                        ArrayPoolEventSource.Log.BucketExhausted(Utilities.GetBucketId(this), _bufferLength, _data.Length, _poolId);
+                    _exhaustedEventSent = true;
+                }
+            }
+            finally
+            {
+                if (taken) _lock.Exit(false);
+            }
+
+            return buffer;
+        }
+
+        /// <summary>
+        /// Attempts to return a Buffer to the bucket. This can fail
+        /// if the buffer being returned was allocated and we don't have
+        /// room for it in the bucket.
+        /// </summary>
+        internal void Return(T[] buffer)
+        {
+            // Check to see if the buffer is the correct size for this bucket
+            if (buffer.Length != _bufferLength)
+                throw new ArgumentException("ArgumentException_BufferNotFromPool", nameof(buffer));
+
+            // Use a SpinLock since it is super lightweight
+            // and our lock is very short lived. Wrap in try-finally
+            // to protect against thread-aborts
+            bool taken = false;
+            try
+            {
+                _lock.Enter(ref taken);
+
+                // If we have space to put the buffer back, do it. If we don't
+                // then there was a buffer alloc'd that was returned instead so
+                // we can just drop this buffer
+                if (_index != 0)
+                {
+                    _data[--_index] = buffer;
+                    _exhaustedEventSent = false; // always setting this should be cheaper than a branch
+                }
+            }
+            finally
+            {
+                if (taken) _lock.Exit(false);
+            }
+        }
+    }
+}
---
+++ tests/Spreads.Core.Tests/NanoOptimizationTests.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Spreads.Serialization;
+
+
+namespace Spreads.Core.Tests {
+
+
+    [TestFixture]
+    public class NanoOptimizationTests {
+
+        [Test, Ignore]
+        public void ArrayVsListVsIListManyTimes() {
+            for (int times = 0; times < 5; times++) {
+                ArrayVsListVsIList();
+            }
+        }
+
+        [Test]
+        public void ArrayVsListVsIList() {
+
+            var len = 10000000;
+
+            var arr = new double[len];
+
+            var buffer = new byte[len * 8];
+            var fixedBuffer = new FixedBuffer(buffer);
+
+            var unmanagedMemory = Marshal.AllocHGlobal(len * 8);
+            var directBuffer = new DirectBuffer(len * 8, unmanagedMemory);
+
+
+            for (int i = 0; i < len; i++) {
+                arr[i] = i;
+                fixedBuffer.WriteDouble(i * 8, i);
+                directBuffer.WriteDouble(i * 8, i);
+            }
+            var list = new List<double>(arr);
+
+            var ilist = list as IList<double>;
+
+
+
+
+
+            var idxs = new int[len / 10];
+            var rng = new System.Random();
+
+            for (int i = 0; i < idxs.Length; i++) {
+                idxs[i] = rng.Next(0, len);
+            }
+
+            var sw = new Stopwatch();
+            var sum = 0.0;
+            sw.Start();
+            sw.Stop();
+
+
+
+
+            sw.Restart();
+            sum = 0.0;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    sum += arr[idx];
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"Array: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+
+            sw.Restart();
+            sum = 0.0;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    sum += list[idx];
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"List: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+            sw.Restart();
+            sum = 0.0;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    lock (list) {
+                        sum += list[idx];
+                    }
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"LockedList: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+            sw.Restart();
+            sum = 0.0;
+            SpinLock sl = new SpinLock();
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    bool gotLock = false;
+                    gotLock = false;
+                    try {
+                        sl.Enter(ref gotLock);
+                        sum += list[idx];
+                    } finally {
+                        // Only give up the lock if you actually acquired it
+                        if (gotLock) sl.Exit();
+                    }
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"SpinLockedList: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+
+            sw.Restart();
+            sum = 0.0;
+            var counter = 0L;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    var dummy = Interlocked.CompareExchange(ref counter, idx, 0L);
+                    sum += list[idx] + dummy;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"InterLockedList: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+            sw.Restart();
+            sum = 0.0;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    sum += ilist[idx];
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"IList: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+            sw.Restart();
+            sum = 0.0;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    sum += fixedBuffer.ReadDouble(idx * 8);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"FixedBuffer: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+            sw.Restart();
+            sum = 0.0;
+            var ifb = fixedBuffer as IDirectBuffer;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    sum += ifb.ReadDouble(idx * 8);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"IFixedBuffer: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+
+            sw.Restart();
+            sum = 0.0;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    sum += directBuffer.ReadDouble(idx * 8);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"DirectBuffer: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+            sw.Restart();
+            sum = 0.0;
+            var idb = directBuffer as IDirectBuffer;
+            for (int rounds = 0; rounds < 10; rounds++) {
+                foreach (var idx in idxs) {
+                    sum += idb.ReadDouble(idx * 8);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"IDirectBuffer: {sum}");
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Mops {(double)len / sw.ElapsedMilliseconds * 0.001}");
+            Console.WriteLine("---");
+
+
+
+        }
+
+
+    }
+}
