59937c4d5354789d2094996b31c6c69e42d46ad2
Victor Baybekov <vbaybekov@gmail.com>
2016-05-01 21:02:00 +0300

    Add interlocked IPC synchronization POC

---
+++ tests/Spreads.Extensions.Tests/IpcListenerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Spreads.Collections.Persistent;
+using Spreads.Storage;
+
+namespace Spreads.Extensions.Tests {
+    [TestFixture]
+    public class IpcListenerTests {
+        [Test]
+        public void CouldReactToChanges() {
+            IpcLongIncrementListener l1;
+            IpcLongIncrementListener l2;
+            var task1 = Task.Run(() => {
+                var cur = 0L;
+                l1 = new IpcLongIncrementListener("../CouldReactToChanges", (lastSeen, current) => {
+                    Console.WriteLine($"Task1: lastSeen {lastSeen} - current {current}");
+                    cur = current;
+                }, 0L);
+                l1.Set(cur);
+                l1.Start();
+                while (cur < 20) {
+                    cur++;
+                    l1.Set(cur);
+                    Thread.Sleep(150);
+                }
+                Thread.Sleep(15);
+            });
+            var task2 = Task.Run(() => {
+                var cur = 0L;
+                l2 = new IpcLongIncrementListener("../CouldReactToChanges", (lastSeen, current) => {
+                    Console.WriteLine($"Task2: lastSeen {lastSeen} - current {current}");
+                    cur = current;
+                }, 0L);
+                l2.Set(cur);
+                l2.Start();
+                while (cur < 20) {
+                    cur++;
+                    l2.Set(cur);
+                    Thread.Sleep(150);
+                }
+                Thread.Sleep(15);
+            });
+            task1.Wait();
+            task2.Wait();
+        }
+
+    }
+}
