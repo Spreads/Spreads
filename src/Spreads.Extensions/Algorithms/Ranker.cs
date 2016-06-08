3f00e11eba17ccae44a69f01602e3df851288ed6
buybackoff <vbaybekov@gmail.com>
2016-06-08 12:13:42 +0300

    Add a generic Ranker that doesn't allocate garbage

---
+++ src/Spreads.Extensions/Algorithms/Ranker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spreads.Collections;
+
+namespace Spreads.Algorithms {
+
+    /// <summary>
+    /// Calculate ranks of values without allocations.
+    /// </summary>
+    public static class Ranker<T> {
+        [ThreadStatic]
+        private static KV<T, int>[] _sorted;
+        [ThreadStatic]
+        private static KV<T, int>[] _ranked;
+        private static KVKeyComparer<T, int> _kvComparer;
+        private static IComparer<T> _comparer;
+
+        /// <summary>
+        /// Simple ascending zero-based rank
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
+        public static ArraySegment<KV<T, int>> SortRank(ArraySegment<T> values, IComparer<T> comparer = null) {
+            KVKeyComparer<T, int> kvComparer;
+            if (_comparer == null) {
+                _comparer = comparer ?? Comparer<T>.Default;
+                _kvComparer = new KVKeyComparer<T, int>(_comparer);
+                kvComparer = _kvComparer;
+            } else if (comparer != null && comparer != _comparer) {
+                kvComparer = new KVKeyComparer<T, int>(comparer);
+            } else {
+                kvComparer = _kvComparer;
+            }
+            if (_sorted == null || values.Count > _sorted.Length) {
+                _sorted = new KV<T, int>[values.Count];
+                _ranked = new KV<T, int>[values.Count];
+            }
+            for (int i = 0; i < values.Count; i++) {
+                _sorted[i] = new KV<T, int>(values.Array[values.Offset + i], i);
+            }
+            Array.Sort(_sorted, kvComparer);
+            for (int i = 0; i < _sorted.Length; i++) {
+                _ranked[_sorted[i].Value] = new KV<T, int>(_sorted[i].Key, i);
+            }
+            return new ArraySegment<KV<T, int>>(_ranked);
+        }
+    }
+}
---
+++ tests/Spreads.Extensions.Tests/Algorithm/RankerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Spreads.Algorithms;
+using Spreads.Collections;
+using Spreads.Storage;
+using Spreads.Storage.Aeron;
+using Spreads.Storage.Aeron.Logbuffer;
+using Spreads.Storage.Aeron.Protocol;
+
+
+namespace Spreads.Extensions.Tests.Algorithm {
+    [TestFixture]
+    public class RankerTests {
+
+        [Test]
+        public void CouldCalculateRankOfArray() {
+            var rng = new System.Random();
+            var values = new double[1000];
+            for (int i = 0; i < values.Length; i++) {
+                values[i] = rng.NextDouble();
+            }
+
+            var result = default(ArraySegment<KV<double, int>>);
+            var sw = new Stopwatch();
+            var gc0 = GC.CollectionCount(0);
+            var gc1 = GC.CollectionCount(0);
+
+            sw.Start();
+            var sum = 0;
+            const int loopCount = 10000;
+            for (int i = 0; i < loopCount; i++) {
+                result = Ranker<double>.SortRank(new ArraySegment<double>(values));
+                sum += result.Array[0].Value;
+            }
+            sw.Stop();
+            gc0 = GC.CollectionCount(0) - gc0;
+            gc1 = GC.CollectionCount(0) - gc1;
+            Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds} msec");
+            Console.WriteLine($"GC0: {gc0}");
+            Console.WriteLine($"GC1: {gc1}");
+            Console.WriteLine($"{values.Length}-sized array per msec: {(double)loopCount / (sw.ElapsedMilliseconds) }");
+
+            for (int i = 0; i < values.Length; i++) {
+                Console.WriteLine($"{result.Array[i].Key} - {result.Array[i].Value}");
+            }
+            Console.WriteLine(sum);
+
+        }
+
+    }
+}
