183cd04c60ed09b8042a13fc69e86d0fec787240
Victor Baybekov <vbaybekov@gmail.com>
2016-07-13 13:51:18 +0300

    Add NuGet packing script

---
+++ nuget/pack.cmd
@@ -0,0 +1,5 @@
+dotnet pack ..\src\Spreads.Core -c RELEASE -o ..\artifacts
+dotnet pack ..\src\Spreads.Collections -c RELEASE -o ..\artifacts
+dotnet pack ..\src\Spreads.Extensions -c RELEASE -o ..\artifacts
+dotnet pack ..\src\Spreads -c RELEASE -o ..\artifacts
+pause
