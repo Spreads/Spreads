130fcb8297650d59ef3d5990ba9bdbcf554942dc
Victor Baybekov <vbaybekov@gmail.com>
2016-10-20 14:48:37 +0300

    Make Spreads a .NET Core C# project and reorder dependencies: Core -> Collections -> Spreads -> Extensions

---
+++ src/Spreads/Spreads.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spreads {
+
+    // settings, etc
+
+    public static class Spreads {
+    }
+}
