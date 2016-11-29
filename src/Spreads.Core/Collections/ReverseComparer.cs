b3694f60777a7849f7c98b05606d89296ed44d17
Victor Baybekov <vbaybekov@gmail.com>
2016-11-29 07:12:00 +0300

    Replace F# port of FixedMinHeap (not used currently) with the original PLINQ's FixedMaxHeap. Min could be simply done with a ReverseComparer.

---
+++ src/Spreads.Core/Collections/ReverseComparer.cs
@@ -0,0 +1,22 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Spreads.Collections {
+
+    public class ReverseComparer<T> : IComparer<T> {
+        private readonly IComparer<T> _comparer;
+
+        public ReverseComparer(IComparer<T> comparer) {
+            _comparer = comparer;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Compare(T x, T y) {
+            return -_comparer.Compare(x, y);
+        }
+    }
+}
