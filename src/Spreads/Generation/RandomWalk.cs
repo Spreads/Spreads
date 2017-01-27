2e2035ba5d9c8af8491dbad3df30fd9b5c90dbf8
Victor Baybekov <vbaybekov@gmail.com>
2017-01-27 19:57:38 +0300

    Add Math.Net as dependency, add RandomWalk series generator (eager SortedMap)

---
+++ src/Spreads/Generation/RandomWalk.cs
@@ -0,0 +1,31 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using MathNet.Numerics.Distributions;
+using Spreads.Collections;
+using System;
+
+namespace Spreads.Generation {
+
+    public static class RandomWalk {
+
+        public static SortedMap<DateTime, double> Generate(DateTime startDate, DateTime endDate, TimeSpan step,
+            double annualizedVolatility, double drift = 0.0, double daysPerYear = 365.25) {
+            if (startDate > endDate) throw new ArgumentException();
+            var capacity = (int)((endDate - startDate).Ticks / step.Ticks);
+            var current = 0.0;
+            var sm = new SortedMap<DateTime, double>(capacity) { { startDate, current } };
+            var tDelta = step.Ticks / (daysPerYear * TimeSpan.TicksPerDay);
+            var driftPerStep = drift * tDelta;
+            var dt = startDate + step;
+            var nd = new Normal(0.0, annualizedVolatility * Math.Sqrt(tDelta));
+            while (dt < endDate) {
+                current += driftPerStep + nd.Sample();
+                dt = dt + step;
+                sm.Add(dt, current);
+            }
+            return sm;
+        }
+    }
+}
--- tests/Spreads.Extensions.Tests/GenerationTests.cs
+++ tests/Spreads.Extensions.Tests/GenerationTests.cs
@@ -2,33 +2,22 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using NUnit.Framework;
-using System.Runtime.InteropServices;
-using Spreads.Collections;
-using Newtonsoft.Json.Bson;
-using Newtonsoft.Json;
-using Spreads;
+using System;
 using System.Threading;
-using System.Threading.Tasks;
+using FSharp.Charting;
+using Microsoft.FSharp.Core;
+using Spreads.Generation;
 
 namespace Spreads.Extensions.Tests {
+
     [TestFixture]
     public class GenerationTests {
-
         private SortedMapGenerator<int> gen;
 
         [Test]
-        public void CouldGenerateSubMillisecondSeries()
-        {
-            
-            gen = new SortedMapGenerator<int>(TimeSpan.TicksPerMillisecond*10, (cnt,prev) => cnt);
+        public void CouldGenerateSubMillisecondSeries() {
+            gen = new SortedMapGenerator<int>(TimeSpan.TicksPerMillisecond * 10, (cnt, prev) => cnt);
             var sm = gen.Generate();
 
             //Task.Run(async () => {
@@ -39,6 +28,16 @@ namespace Spreads.Extensions.Tests {
             //});
 
             Thread.Sleep(1500);
+            Console.WriteLine("Generated number of values: {0}", sm.Count);
+            //gen = null;
+        }
+
+        [Test]
+        public void CouldGenerateRandomWalk() {
+            var sm = RandomWalk.Generate(DateTime.Today.AddYears(-1), DateTime.Today, TimeSpan.FromDays(1), 0.20, 0.15);
+
+            Thread.Sleep(1500);
+            Console.WriteLine($"Last: {sm.Last.Value}");
             Console.WriteLine("Generated number of values: {0}", sm.Count);
             //gen = null;
         }
