dec012f9c2292c1cd69f8c060884119abf87c83b
Victor Baybekov <vbaybekov@gmail.com>
2017-02-08 11:41:42 +0300

    add SM binary serialization test

--- tests/Spreads.Extensions.Tests/SerializationTests.cs
+++ tests/Spreads.Extensions.Tests/SerializationTests.cs
@@ -4,6 +4,7 @@
 
 
 using System;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using NUnit.Framework;
@@ -29,6 +30,27 @@ namespace Spreads.Extensions.Tests {
             var str = JsonConvert.SerializeObject(sm);
             Console.WriteLine(str);
             var sm2 = JsonConvert.DeserializeObject<SortedMap<DateTime, double>>(str);
+            Assert.IsTrue(sm.SequenceEqual(sm2));
+        }
+
+        [Test]
+        public void CouldSerializeSortedMapWithBinary()
+        {
+            SortedMap<DateTime, double>.Init();
+            var sm = new SortedMap<DateTime, double>();
+            for (int i = 0; i < 10; i++) {
+                sm.Add(DateTime.UtcNow.Date.AddDays(i), i);
+            }
+            MemoryStream tmp;
+            var len = BinarySerializer.SizeOf(sm, out tmp);
+            Console.WriteLine(len);
+            var dest = BufferPool.PreserveMemory(len);
+            var len2 = BinarySerializer.Write(sm, ref dest, 0, tmp);
+            Assert.AreEqual(len, len2);
+
+            SortedMap<DateTime, double> sm2 = null;
+            BinarySerializer.Read<SortedMap<DateTime, double>>(dest, 0, ref sm2);
+
             Assert.IsTrue(sm.SequenceEqual(sm2));
         }
 
