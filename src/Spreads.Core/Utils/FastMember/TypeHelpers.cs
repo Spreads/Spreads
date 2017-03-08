3e0863457bb7440b8a06794fbcc6f5cf62a0cd00
Victor Baybekov <vbaybekov@gmail.com>
2017-03-08 23:03:00 +0300

    Add FastMember as source since NuGet package lags master and doesn't have useful stuff such as Member.GetAttribute

---
+++ src/Spreads.Core/Utils/FastMember/TypeHelpers.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) 2017, Marc Gravell https://github.com/mgravell/fast-member
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Spreads.Utils.FastMember
+{
+
+    internal static class TypeHelpers
+    {
+#if COREFX
+        public static readonly Type[] EmptyTypes = new Type[0];
+#else
+        public static readonly Type[] EmptyTypes = Type.EmptyTypes;
+#endif
+
+        public static bool _IsValueType(Type type)
+        {
+#if COREFX
+            return type.GetTypeInfo().IsValueType;
+#else
+            return type.IsValueType;
+#endif
+        }
+        public static bool _IsPublic(Type type)
+        {
+#if COREFX
+            return type.GetTypeInfo().IsPublic;
+#else
+            return type.IsPublic;
+#endif
+        }
+
+        public static bool _IsNestedPublic(Type type)
+        {
+#if COREFX
+            return type.GetTypeInfo().IsNestedPublic;
+#else
+            return type.IsNestedPublic;
+#endif
+        }
+        public static bool _IsClass(Type type)
+        {
+#if COREFX
+            return type.GetTypeInfo().IsClass;
+#else
+            return type.IsClass;
+#endif
+        }
+
+        public static bool _IsAbstract(Type type)
+        {
+#if COREFX
+            return type.GetTypeInfo().IsAbstract;
+#else
+            return type.IsAbstract;
+#endif
+        }
+        public static Type _CreateType(TypeBuilder type)
+        {
+#if COREFX
+            return type.CreateTypeInfo().AsType();
+#else
+            return type.CreateType();
+#endif
+        }
+
+        public static int Min(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+    }
+}
