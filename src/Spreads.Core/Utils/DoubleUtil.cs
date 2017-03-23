942c8c49ed50cc5b7dcffef3e0c8b6a83f7803a4
Victor Baybekov <vbaybekov@gmail.com>
2017-03-23 17:12:03 +0300

    Interesting experiment to compare three calculations of double.IsOppositeSign. Bitwise and Product < 0 are the same. Another observation is that I have spent on even this simple test probably more than I could ever save cumulatively in total to calculate this thing :)
    Elapsed BitWise: 747
    Elapsed Product < 0: 751
    Elapsed Math.Sign: 1173
    Elapsed BitWise: 728
    Elapsed Product < 0: 763
    Elapsed Math.Sign: 1161
    Elapsed BitWise: 743
    Elapsed Product < 0: 764
    Elapsed Math.Sign: 1152

---
+++ src/Spreads.Core/Utils/DoubleUtil.cs
@@ -0,0 +1,46 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spreads.Utils
+{
+    public static class DoubleUtil
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsOppositeSignNaive(this double first, double second)
+        {
+            var sign1 = Math.Sign(first);
+            var sign2 = Math.Sign(second);
+            return sign1 * sign2 == -1;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsOppositeSign(this double first, double second)
+        {
+            return first * second < 0.0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe bool IsOppositeSignBitwise(this double first, double second)
+        {
+            const ulong mask = (1UL << 63) - 1UL;
+            unsafe
+            {
+                var value1 = *((ulong*)(&first));
+                var value2 = *((ulong*)(&second));
+                //var signBitDiffers = (value1 >> 63) != (value2 >> 63);
+                //var firstIsNonZero = (mask & value1) != 0UL;
+                //var secondIsNonZero = (mask & value2) != 0UL;
+                return ((value1 >> 63) != (value2 >> 63)) && ((mask & value1) != 0UL) && ((mask & value2) != 0UL);
+            }
+        }
+    }
+}
---
+++ tests/Spreads.Core.Tests/DoubleUtilTests.cs
@@ -0,0 +1,93 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Spreads.Utils;
+
+namespace Spreads.Core.Tests
+{
+    [TestFixture]
+    public class DoubleUtilTests
+    {
+        [Test, Ignore]
+        public void CompareOppositeSignSpeed()
+        {
+            var doubles = new double[10000000];
+            var rng = new System.Random();
+            doubles[0] = 0.0;
+            doubles[1] = -0.0;
+            for (int i = 2; i < doubles.Length; i++)
+            {
+                doubles[i] = rng.NextDouble() - 0.5;
+            }
+
+            for (int i = 1; i < doubles.Length; i++)
+            {
+
+                var isOpposite = doubles[i].IsOppositeSign(doubles[i - 1]);
+                var isOpposite2 = doubles[i].IsOppositeSignBitwise(doubles[i - 1]);
+                var isOpposite3 = doubles[i].IsOppositeSignNaive(doubles[i - 1]);
+                if ((isOpposite != isOpposite2) | (isOpposite != isOpposite3))
+                {
+                    Assert.Fail($"Wrong opposites: MS {isOpposite} vs BW {isOpposite2} for {doubles[i]} and {doubles[i - 1]}");
+                }
+            }
+
+            var count1 = 0L;
+            var count2 = 0L;
+            var count3 = 0L;
+            var sw = new Stopwatch();
+            for (int rr = 0; rr < 10; rr++)
+            {
+                sw.Restart();
+                for (int r = 0; r < 10; r++)
+                {
+                    for (int i = 1; i < doubles.Length; i++)
+                    {
+                        var isOpposite = doubles[i].IsOppositeSign(doubles[i - 1]);
+                        if (isOpposite) count1++;
+                    }
+                }
+                sw.Stop();
+
+                Console.WriteLine($"Elapsed Product < 0: {sw.ElapsedMilliseconds}");
+
+                sw.Restart();
+                for (int r = 0; r < 10; r++)
+                {
+                    for (int i = 1; i < doubles.Length; i++)
+                    {
+                        var isOpposite = doubles[i].IsOppositeSignNaive(doubles[i - 1]);
+                        if (isOpposite) count2++;
+                    }
+                }
+                sw.Stop();
+
+                Console.WriteLine($"Elapsed Math.Sign: {sw.ElapsedMilliseconds}");
+
+                sw.Restart();
+                for (int r = 0; r < 10; r++)
+                {
+                    for (int i = 1; i < doubles.Length; i++)
+                    {
+                        var isOpposite = doubles[i].IsOppositeSign(doubles[i - 1]);
+                        if (isOpposite) count3++;
+                    }
+                }
+                sw.Stop();
+
+                Console.WriteLine($"Elapsed BitWise: {sw.ElapsedMilliseconds}");
+            }
+            Console.WriteLine(count1);
+            Console.WriteLine(count2);
+            Assert.AreEqual(count1, count2);
+        }
+    }
+}
