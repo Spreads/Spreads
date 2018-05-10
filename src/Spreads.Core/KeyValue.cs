a57e1a55af85eab7421c4bdca98a3465af741b34
buybackoff <vbaybekov@gmail.com>
2018-05-10 11:38:10 +0300

    Make KeyValue normal struct

--- src/Spreads.Core/KeyValue.cs
+++ src/Spreads.Core/KeyValue.cs
@@ -8,6 +8,55 @@ using SRCS = System.Runtime.CompilerServices;
 
 namespace Spreads
 {
+    public enum KeyValuePresence
+    {
+        BothMissing = 0,
+        BothPresent = -1,
+        KeyMissing = -2, // E.g. for Fill(x) we do not calculate where the previous key was, just check if it exists and if not return given value.
+        ValueMissing = -3,
+    }
+
+
+    public static class KeyValue
+    {
+
+        public static KeyValue<TKey, TValue> Create<TKey, TValue>(in TKey key, in TValue value)
+        {
+            return new KeyValue<TKey, TValue>(in key, in value);
+        }
+
+        public static KeyValue<TKey, TValue> Create<TKey, TValue>(in TKey key, in TValue value, in long version)
+        {
+            return new KeyValue<TKey, TValue>(in key, in value, in version);
+        }
+
+        public static KeyValue<TKey, TValue> OnlyKey<TKey, TValue>(in TKey key)
+        {
+            return new KeyValue<TKey, TValue>(in key, default, -3, KeyValuePresence.ValueMissing);
+        }
+
+        public static KeyValue<TKey, TValue> OnlyKey<TKey, TValue>(in TKey key, in long version)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static KeyValue<TKey, TValue> OnlyValue<TKey, TValue>(in TValue value)
+        {
+            return new KeyValue<TKey, TValue>(default, default, -2, KeyValuePresence.KeyMissing);
+        }
+
+        public static KeyValue<TKey, TValue> OnlyValue<TKey, TValue>(in TValue value, in long version)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+
+
+    // NB `in` in ctor gives 95% of perf gain, not `ref struct`, but keep it as ref struct for semantics and potential upgrade to ref fields when they are implemented
+    // Also it's fatter due to version so it's better to restrict saving it in arrays/fields.
+
     // TODO xml docs
     /// <summary>
     /// Stack-only struct that represents references to a key and value pair. It's like an
@@ -18,15 +67,18 @@ namespace Spreads
     /// </summary>
     /// <typeparam name="TKey"></typeparam>
     /// <typeparam name="TValue"></typeparam>
-    public readonly unsafe ref struct KeyValue<TKey, TValue>
+    public struct KeyValue<TKey, TValue>
     {
         // NB See https://github.com/dotnet/csharplang/issues/1147
         // and https://github.com/dotnet/corert/blob/796aeaa64ec09da3e05683111c864b529bcc17e8/src/System.Private.CoreLib/src/System/ByReference.cs
-        // Use it when it is made public
+        // Try using it when it is made public
 
-        private readonly IntPtr _kp;
-
-        private readonly IntPtr _vp;
+        // ReSharper disable InconsistentNaming
+        // NB could be used from cursors
+        internal TKey _k;
+        internal TValue _v;
+        internal long _version;
+        // ReSharper restore InconsistentNaming
 
         public TKey Key
         {
@@ -36,8 +88,7 @@ namespace Spreads
 #if DEBUG
                 if (IsMissing) { throw new NullReferenceException("Must check IsMissing property of KeyValue before accesing Key/Value if unsure that a value exists"); }
 #endif
-                // NB On x86_64 no visible perf diff, use Unaligned te be safe than sorry later
-                return SRCS.Unsafe.ReadUnaligned<TKey>((void*)_kp);
+                return _k;
             }
         }
 
@@ -49,8 +100,7 @@ namespace Spreads
 #if DEBUG
                 if (IsMissing) { throw new NullReferenceException("Must check IsMissing property of KeyValue before accesing Key/Value if unsure that a value exists"); }
 #endif
-                // NB On x86_64 no visible perf diff, use Unaligned te be safe than sorry later
-                return SRCS.Unsafe.ReadUnaligned<TValue>((void*)_vp);
+                return _v;
             }
         }
 
@@ -59,7 +109,7 @@ namespace Spreads
             [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
             get
             {
-                return _kp == IntPtr.Zero && _vp == IntPtr.Zero;
+                return _version == 0;
             }
         }
 
@@ -68,38 +118,66 @@ namespace Spreads
             [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
             get
             {
-                return !(_kp == IntPtr.Zero && _vp == IntPtr.Zero);
+                return !IsMissing;
+            }
+        }
+
+        public KeyValuePresence Presence
+        {
+            [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                if (_version == -1 || _version > 0)
+                {
+                    return KeyValuePresence.BothPresent;
+                }
+                return (KeyValuePresence)_version;
+            }
+        }
+
+        public long Version
+        {
+            [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                return _version;
             }
         }
 
         [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
         public KeyValue(in TKey k, in TValue v)
         {
-            _kp = (IntPtr)SRCS.Unsafe.AsPointer(ref SRCS.Unsafe.AsRef(k));
-            _vp = (IntPtr)SRCS.Unsafe.AsPointer(ref SRCS.Unsafe.AsRef(v));
+            _k = k;
+            _v = v;
+            _version = -1;
         }
 
         [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
-        public KeyValue(TKey k, TValue v)
+        public KeyValue(in TKey k, in TValue v, in long version)
         {
-            _kp = (IntPtr)SRCS.Unsafe.AsPointer(ref k);
-            _vp = (IntPtr)SRCS.Unsafe.AsPointer(ref v);
+            if (version <= 0)
+            {
+                ThrowHelper.ThrowArgumentException("Version is zero or negative!");
+            }
+            _k = k;
+            _v = v;
+            _version = version;
         }
 
         [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
-        public KeyValue(KeyValuePair<TKey, TValue> kvp)
+        internal KeyValue(in TKey k, in TValue v, in long version, in KeyValuePresence presense)
         {
-            var k = kvp.Key;
-            var v = kvp.Value;
-            _kp = (IntPtr)SRCS.Unsafe.AsPointer(ref k);
-            _vp = (IntPtr)SRCS.Unsafe.AsPointer(ref v);
+            // TODO bit flags for presense, int62 shall be enough for everyone
+            _k = k;
+            _v = v;
+            _version = version;
         }
 
         // TODO make implicit after refactoring all projetcs
         [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
         public static explicit operator KeyValue<TKey, TValue>(KeyValuePair<TKey, TValue> kvp)
         {
-            return new KeyValue<TKey, TValue>(kvp);
+            return new KeyValue<TKey, TValue>(kvp.Key, kvp.Value);
         }
 
         [SRCS.MethodImpl(SRCS.MethodImplOptions.AggressiveInlining)]
