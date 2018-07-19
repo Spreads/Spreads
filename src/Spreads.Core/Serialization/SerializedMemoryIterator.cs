ee4b0eb2e1742558ee10d678b3894abad03798a1
buybackoff <vbaybekov@gmail.com>
2018-07-19 20:58:56 +0300

    Add SerializedMemoryIterator

---
+++ src/Spreads.Core/Serialization/SerializedMemoryIterator.cs
@@ -0,0 +1,145 @@
+﻿using Spreads.Buffers;
+using Spreads.DataTypes;
+using Spreads.Serialization.Utf8Json;
+using System;
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static System.Runtime.CompilerServices.Unsafe;
+
+namespace Spreads.Serialization
+{
+    // Read indivual values from serilized arrays without deserialization
+
+    public unsafe struct SerializedMemoryIterator : IEnumerable<DirectBuffer>, IEnumerator<DirectBuffer>
+    {
+        private readonly Memory<byte> _memory;
+        private readonly bool _isBinary;
+        private MemoryHandle _handle;
+
+        // private int _index;
+        private int _offset;
+
+        private int _length;
+        private JsonReader _reader;
+
+        public SerializedMemoryIterator(Memory<byte> memory, bool isBinary)
+        {
+            _memory = memory;
+            _isBinary = isBinary;
+
+            _handle = memory.Pin();
+
+            // _index = -1;
+            _offset = 0;
+            _length = 0;
+            if (!isBinary)
+            {
+                if (!MemoryMarshal.TryGetArray<byte>(memory, out var segment))
+                {
+                    ThrowHelper.ThrowInvalidOperationException("JSON data must be currently backed by a byte array. This is Utf8Json limitation.");
+                }
+
+                _reader = new JsonReader(segment.Array, segment.Offset);
+            }
+            else
+            {
+                _reader = default;
+            }
+        }
+
+        public SerializedMemoryIterator GetEnumerator()
+        {
+            return new SerializedMemoryIterator(_memory, _isBinary);
+        }
+
+        IEnumerator<DirectBuffer> IEnumerable<DirectBuffer>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool MoveNext()
+        {
+            if (_isBinary)
+            {
+                // binary has always a header with fixed-size or an int after the header with payload size
+                // note that this only supports individual values packed consequitively, not binary arrays
+
+                _offset = _offset + _length;
+
+                if (_offset >= _memory.Length)
+                {
+                    return false;
+                }
+
+                var header = ReadUnaligned<DataTypeHeader>(_handle.Pointer);
+                if (header.IsFixedSize)
+                {
+                    _length = DataTypeHeader.Size + header.TypeSize;
+                }
+                else
+                {
+                    var payloadLength = ReadUnaligned<int>((void*)((IntPtr)_handle.Pointer + 4));
+                    _length = DataTypeHeader.Size + 4 + payloadLength;
+                }
+
+                return true;
+            }
+            else
+            {
+                // segment is what is returned by ReadNextBlock
+                if (_offset == 0)
+                {
+                    _reader.ReadIsBeginArrayWithVerify();
+                    var subsegment = _reader.ReadNextBlockSegment();
+                    _offset = subsegment.Offset;
+                    _length = subsegment.Count;
+                    return true;
+                }
+
+                if (_reader.ReadIsValueSeparator())
+                {
+                    if (_reader.ReadIsEndArray()) // [x, y, z,] - case of end separator
+                    {
+                        Trace.TraceWarning("We should not support this case when packing data");
+                        return false;
+                    }
+                    var subsegment = _reader.ReadNextBlockSegment();
+                    _offset = subsegment.Offset;
+                    _length = subsegment.Count;
+                    return true;
+                }
+
+                _reader.ReadIsEndArrayWithVerify();
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            _offset = 0;
+        }
+
+        public DirectBuffer Current
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get { return new DirectBuffer(_length, (IntPtr)_handle.Pointer + _offset); }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            _handle.Dispose();
+        }
+    }
+}
---
+++ tests/Spreads.Core.Tests/Serialization/SerializedMemoryIteratorTests.cs
@@ -0,0 +1,50 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using NUnit.Framework;
+using Spreads.Serialization;
+using Spreads.Serialization.Utf8Json;
+using Spreads.Utils;
+using System;
+
+namespace Spreads.Core.Tests.Serialization
+{
+    [TestFixture]
+    public class SerializedMemoryIteratorTests
+    {
+        public Random Random { get; set; } = new Random();
+
+        [Test]
+        public void CouldIterateOverSerializedJson()
+        {
+            var count = 1_00_000;
+
+            var values = new double[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = Random.NextDouble();
+            }
+
+            var bytes = JsonSerializer.Serialize(values);
+
+            using (Benchmark.Run("SerializedMemoryIterator JSON", count))
+            {
+                var iterator = new SerializedMemoryIterator(bytes, false);
+
+                var cnt = 0;
+                foreach (var db in iterator)
+                {
+                    var value = JsonSerializer.Deserialize<double>(db.Span.ToArray());
+                    if (Math.Abs(values[cnt] - value) > 0.000000001)
+                    {
+                        Assert.Fail("Values are not equal");
+                    }
+                    cnt++;
+                }
+            }
+            Benchmark.Dump();
+        }
+    }
+}
