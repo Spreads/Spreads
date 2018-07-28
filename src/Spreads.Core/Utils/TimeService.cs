9025008b74d1c2ea254cafe1e9624d1451a69507
buybackoff <vbaybekov@gmail.com>
2018-07-28 16:52:07 +0300

    Add TimeService that produces always unique incrementing by a nanosecond time stamp, updates timestamp to high resolution time (DateTime.UtcNow) every Timer tick (set to 1 msec, but actual is c.15msec on Windows). It stores timestamp in IntPtr that could be on shared memory. On muticore machines a dedicated thread could call UpdateTime in a loop. If there is a higher precision time that DateTime.UtcNow then one could just set the pointer value directly. Property CurrentTime just atomically increments the value at pointer by 1 nanosecond while access time is c.6 nanos, so it's OK and we cannot overrun actual time.

---
+++ src/Spreads.Core/Utils/TimeService.cs
@@ -0,0 +1,78 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using Spreads.DataTypes;
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Spreads.Utils
+{
+    public static unsafe class TimeService
+    {
+        private static IntPtr _lastUpdatedPtr;
+        private static bool _allocated;
+
+        private static Timer _timer;
+
+        static TimeService()
+        {
+            // Could stop and start with another pointer if needed
+            Start();
+        }
+
+        public static void Start()
+        {
+            Start(IntPtr.Zero);
+        }
+
+        public static void Start(IntPtr ptr)
+        {
+            if (_lastUpdatedPtr != IntPtr.Zero)
+            {
+                Stop();
+            }
+
+            if (ptr == IntPtr.Zero)
+            {
+                ptr = Marshal.AllocHGlobal(8);
+                _allocated = true;
+            }
+
+            _lastUpdatedPtr = ptr;
+
+            _timer = new Timer(o =>
+            {
+                UpdateTime();
+            }, null, 0, 1);
+            UpdateTime();
+        }
+
+        public static void Stop()
+        {
+            if (_lastUpdatedPtr != IntPtr.Zero)
+            {
+                _timer.Dispose();
+                if (_allocated)
+                {
+                    Marshal.FreeHGlobal(_lastUpdatedPtr);
+                }
+                _lastUpdatedPtr = IntPtr.Zero;
+            }
+        }
+
+        public static Timestamp CurrentTime
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get { return (Timestamp)Interlocked.Add(ref *(long*)_lastUpdatedPtr, 1); }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void UpdateTime()
+        {
+            Interlocked.Exchange(ref *(long*)_lastUpdatedPtr, (long)(Timestamp)DateTime.UtcNow);
+        }
+    }
+}
---
+++ tests/Spreads.Core.Tests/Utils/TimeService.cs
@@ -0,0 +1,72 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+using Spreads.Utils;
+
+namespace Spreads.Core.Tests.Utils
+{
+    [TestFixture]
+    public class TimeServiceTests
+    {
+        [Test]
+        [Explicit("long running")]
+        public void TimeServiceProducesUniqueValues()
+        {
+            // multi calls are ok
+            TimeService.Start();
+            TimeService.Stop();
+            TimeService.Stop();
+            TimeService.Start();
+            TimeService.Start();
+
+            var ptr = Marshal.AllocHGlobal(8);
+            TimeService.Stop();
+            TimeService.Start(ptr);
+
+
+            var previous = TimeService.CurrentTime;
+            for (int i = 0; i < 1_00_000_000; i++)
+            {
+                var current = TimeService.CurrentTime;
+                if (current <= previous)
+                {
+                    Assert.Fail();
+                }
+
+                var delta = current.Nanos - previous.Nanos;
+                if (delta > 1)
+                {
+                    Console.WriteLine($"{current.Nanos} - {delta}");
+                }
+
+                if (i % 50_000_000 == 0)
+                {
+                    TimeService.UpdateTime();
+                }
+                previous = current;
+            }
+        }
+
+
+        [Test]
+        [Explicit("long running")]
+        public void TimeServiceGetterBenchmark()
+        {
+            var ptr = Marshal.AllocHGlobal(8);
+            // TimeService.Start(ptr);
+            var count = 1_000_000_000;
+            using (Benchmark.Run("TimeServiceGetter", count))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    var current = TimeService.CurrentTime;
+                }
+            }
+            
+        }
+    }
+}
