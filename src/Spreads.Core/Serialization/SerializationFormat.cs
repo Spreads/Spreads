7fdca74ee61c4891142ee1ab1cd0bc45f0aebacc
buybackoff <vbaybekov@gmail.com>
2018-08-09 11:03:45 +0300

    Add remarks on difference between binary SerializationFormat and transport/protocol format. Binary could be Json with a header, potentially even the same data type could be serialized as blittable or as Json, yet this should not be supported supported inside a single strongly-typed data stream.

--- src/Spreads.Core/Serialization/SerializationFormat.cs
+++ src/Spreads.Core/Serialization/SerializationFormat.cs
@@ -1,20 +1,36 @@
 ﻿namespace Spreads.Serialization
 {
     /// <summary>
+    /// Binary serialization format. Serialized data always has a header describing the method of
+    /// binary serialization. The data could be Json, but because of the header it is not directly
+    /// consumable by e.g. browsers. However, for JSON/JSONDeflate cases the only difference is
+    /// the header and the reast payload could be directly sent to a browser expecting 'application/json'
+    /// MIME type.
+    /// </summary>
+    /// <remarks>
     /// JsonDeflate:
     /// Objects are converted to JSON and compressed with raw deflate method
     /// if compression gives smaller size. This format is compatible with raw
-    /// http payload with 'Accept-encoding: deflate' headers.
+    /// http payload with 'Accept-encoding: deflate' headers past the 8 bytes header.
     ///
     /// BinaryLz4/Zstd:
     /// Binary is stored as blittable representation with Blosc byteshuffle for arrays with fixed-size elements.
-    /// Then serialized (and byteshuffled) buffer is compressed with LZ4 (super fast) or Zstd (high compression)
-    /// if compression gives smaller size. Actual buffer layout is type dependent.
+    /// Then serialized (and byteshuffled) buffer is compressed with LZ4 (super fast but lower compression) or
+    /// Zstd (still fast and high compression, preferred method) if compression gives smaller size.
+    /// Actual buffer layout is type dependent.
     ///
-    /// Serialized buffer should have a header with a flag indicating the format and
+    /// Serialized buffer has a header with a flag indicating the format and
     /// wether or not the buffer is compressed. Some data types are stored as deltas
     /// with diffing performed before byteshuffling and the header must have such flag as well.
-    /// </summary>
+    ///
+    /// SerializationFormat should not be confused with Transport/Protocol format:
+    /// * Text trasport/protocol supports only JSON and the payload is plain or deflated JSON that
+    ///   could be consumed by browsers or http servers (with headers indicating the actual format - compressed or not).
+    /// * Binary trasport/protocol cannot be consumed directly by browsers or http servers. The exact data layout of payload
+    ///   depends on data type and is encoded in a 4 (+ optional 4 bytes length for variable sized data) bytes header.
+    ///   Data could be serialized as JSON (mostly for convenience if a type is not used a lot and not blittable)
+    ///   but it is still a binary format, though one that could be trivially converted to JSON.
+    /// </remarks>
     public enum SerializationFormat : byte
     {
         /// <summary>
