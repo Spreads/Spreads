ba3008764e5fe87414ef643e8a9454681c35b9a7
buybackoff <vbaybekov@gmail.com>
2018-08-18 12:36:51 +0300

    Add Symbol struct with sizes 32, 64, 128, 256. Remove template codegen.

--- src/Spreads.Core/DataTypes/Symbol.cs
+++ src/Spreads.Core/DataTypes/Symbol.cs
@@ -16,14 +16,8 @@ namespace Spreads.DataTypes
     // why this has a lot of sense in some cases: on x64 a string takes 26 + length * 2,
     // so we always win for small strings even with padding.
 
-    // TODO Watch for usage of this. This is convenient when reading/writing a binary stream from a wire, e.g. when we
-    // know that a ticker has max length. But in many cases they could be manually "interned", or there could be a special
-    // class Symbol with additional info and a concurrent dictionary that indexes Symbol by string.
-    // Then we could store a 8-byte pointer instead of 16-byte symbol (or even an int and have another dictionary). Many tradeoffs...
-    // Also see String.Intern method
-
     /// <summary>
-    /// A struct to store up to 16 chars.
+    /// A struct to store up to 16 UTF8 chars.
     /// </summary>
     [DebuggerDisplay("{" + nameof(ToString) + "()}")]
     [StructLayout(LayoutKind.Sequential, Size = Size)]
@@ -64,7 +58,7 @@ namespace Spreads.DataTypes
         {
             var buffer = BufferPool.StaticBuffer.Memory;
             var len = 0;
-
+            var span = buffer.Span;
             var ptr = (byte*)Unsafe.AsPointer(ref this);
             for (int i = 0; i < Size; i++)
             {
@@ -73,7 +67,6 @@ namespace Spreads.DataTypes
                 {
                     break;
                 }
-                var span = buffer.Span;
                 span[i] = b;
                 len = i + 1;
             }
@@ -81,6 +74,7 @@ namespace Spreads.DataTypes
             // TODO use CoreFxLab new encoding features
             if (MemoryMarshal.TryGetArray((ReadOnlyMemory<byte>)buffer, out var segment))
             {
+                // ReSharper disable once AssignNullToNotNullAttribute
                 return Encoding.UTF8.GetString(segment.Array, segment.Offset, len);
             }
             else
@@ -117,6 +111,369 @@ namespace Spreads.DataTypes
         /// Not equals operator.
         /// </summary>
         public static bool operator !=(Symbol x, Symbol y)
+        {
+            return !x.Equals(y);
+        }
+
+        /// <summary>
+        /// Get Symbol as bytes Span.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan()
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return new Span<byte>(ptr, Size);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void ThrowArgumentOutOfRangeException(string argumentName)
+        {
+            throw new ArgumentOutOfRangeException(argumentName, "Symbol length is too large");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void ThrowApplicationException()
+        {
+            throw new ApplicationException();
+        }
+    }
+
+    /// <summary>
+    /// A struct to store up to 64 UTF8 chars.
+    /// </summary>
+    [DebuggerDisplay("{" + nameof(ToString) + "()}")]
+    [StructLayout(LayoutKind.Sequential, Size = Size)]
+    [Serialization(BlittableSize = Size)]
+    public unsafe struct Symbol64 : IEquatable<Symbol64>
+    {
+        private const int Size = 64;
+        private fixed byte Bytes[Size];
+
+        /// <summary>
+        /// Symbol constructor.
+        /// </summary>
+        public Symbol64(string symbol)
+        {
+            var byteCount = Encoding.UTF8.GetByteCount(symbol);
+            if (byteCount > Size)
+            {
+                ThrowArgumentOutOfRangeException(nameof(symbol));
+            }
+            fixed (char* charPtr = symbol)
+            fixed (byte* ptr = Bytes)
+            {
+                Encoding.UTF8.GetBytes(charPtr, symbol.Length, ptr, Size);
+            }
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(Symbol64 other)
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return *(long*)(ptr) == *(long*)(other.Bytes) && *(long*)((byte*)ptr + 8) == *(long*)(other.Bytes + 8);
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            var buffer = BufferPool.StaticBuffer.Memory;
+            var len = 0;
+            var span = buffer.Span;
+            var ptr = (byte*)Unsafe.AsPointer(ref this);
+            for (int i = 0; i < Size; i++)
+            {
+                var b = *(ptr + i);
+                if (b == 0)
+                {
+                    break;
+                }
+                span[i] = b;
+                len = i + 1;
+            }
+
+            // TODO use CoreFxLab new encoding features
+            if (MemoryMarshal.TryGetArray((ReadOnlyMemory<byte>)buffer, out var segment))
+            {
+                // ReSharper disable once AssignNullToNotNullAttribute
+                return Encoding.UTF8.GetString(segment.Array, segment.Offset, len);
+            }
+            else
+            {
+                ThrowApplicationException();
+                return String.Empty;
+            }
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Symbol64 && Equals((Symbol64)obj);
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode()
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return *(int*)ptr;
+        }
+
+        /// <summary>
+        /// Equals operator.
+        /// </summary>
+        public static bool operator ==(Symbol64 x, Symbol64 y)
+        {
+            return x.Equals(y);
+        }
+
+        /// <summary>
+        /// Not equals operator.
+        /// </summary>
+        public static bool operator !=(Symbol64 x, Symbol64 y)
+        {
+            return !x.Equals(y);
+        }
+
+        /// <summary>
+        /// Get Symbol as bytes Span.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan()
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return new Span<byte>(ptr, Size);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void ThrowArgumentOutOfRangeException(string argumentName)
+        {
+            throw new ArgumentOutOfRangeException(argumentName, "Symbol length is too large");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void ThrowApplicationException()
+        {
+            throw new ApplicationException();
+        }
+    }
+
+    /// <summary>
+    /// A struct to store up to 128 UTF8 chars.
+    /// </summary>
+    [DebuggerDisplay("{" + nameof(ToString) + "()}")]
+    [StructLayout(LayoutKind.Sequential, Size = Size)]
+    [Serialization(BlittableSize = Size)]
+    public unsafe struct Symbol128 : IEquatable<Symbol128>
+    {
+        private const int Size = 128;
+        private fixed byte Bytes[Size];
+
+        /// <summary>
+        /// Symbol constructor.
+        /// </summary>
+        public Symbol128(string symbol)
+        {
+            var byteCount = Encoding.UTF8.GetByteCount(symbol);
+            if (byteCount > Size)
+            {
+                ThrowArgumentOutOfRangeException(nameof(symbol));
+            }
+            fixed (char* charPtr = symbol)
+            fixed (byte* ptr = Bytes)
+            {
+                Encoding.UTF8.GetBytes(charPtr, symbol.Length, ptr, Size);
+            }
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(Symbol128 other)
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return *(long*)(ptr) == *(long*)(other.Bytes) && *(long*)((byte*)ptr + 8) == *(long*)(other.Bytes + 8);
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            var buffer = BufferPool.StaticBuffer.Memory;
+            var len = 0;
+            var span = buffer.Span;
+            var ptr = (byte*)Unsafe.AsPointer(ref this);
+            for (int i = 0; i < Size; i++)
+            {
+                var b = *(ptr + i);
+                if (b == 0)
+                {
+                    break;
+                }
+                span[i] = b;
+                len = i + 1;
+            }
+
+            // TODO use CoreFxLab new encoding features
+            if (MemoryMarshal.TryGetArray((ReadOnlyMemory<byte>)buffer, out var segment))
+            {
+                // ReSharper disable once AssignNullToNotNullAttribute
+                return Encoding.UTF8.GetString(segment.Array, segment.Offset, len);
+            }
+            else
+            {
+                ThrowApplicationException();
+                return String.Empty;
+            }
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Symbol128 && Equals((Symbol128)obj);
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode()
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return *(int*)ptr;
+        }
+
+        /// <summary>
+        /// Equals operator.
+        /// </summary>
+        public static bool operator ==(Symbol128 x, Symbol128 y)
+        {
+            return x.Equals(y);
+        }
+
+        /// <summary>
+        /// Not equals operator.
+        /// </summary>
+        public static bool operator !=(Symbol128 x, Symbol128 y)
+        {
+            return !x.Equals(y);
+        }
+
+        /// <summary>
+        /// Get Symbol as bytes Span.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan()
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return new Span<byte>(ptr, Size);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void ThrowArgumentOutOfRangeException(string argumentName)
+        {
+            throw new ArgumentOutOfRangeException(argumentName, "Symbol length is too large");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void ThrowApplicationException()
+        {
+            throw new ApplicationException();
+        }
+    }
+
+    /// <summary>
+    /// A struct to store up to 256 UTF8 chars.
+    /// </summary>
+    [DebuggerDisplay("{" + nameof(ToString) + "()}")]
+    [StructLayout(LayoutKind.Sequential, Size = Size)]
+    [Serialization(BlittableSize = Size)]
+    public unsafe struct Symbol256 : IEquatable<Symbol256>
+    {
+        private const int Size = 256;
+        private fixed byte Bytes[Size];
+
+        /// <summary>
+        /// Symbol constructor.
+        /// </summary>
+        public Symbol256(string symbol)
+        {
+            var byteCount = Encoding.UTF8.GetByteCount(symbol);
+            if (byteCount > Size)
+            {
+                ThrowArgumentOutOfRangeException(nameof(symbol));
+            }
+            fixed (char* charPtr = symbol)
+            fixed (byte* ptr = Bytes)
+            {
+                Encoding.UTF8.GetBytes(charPtr, symbol.Length, ptr, Size);
+            }
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(Symbol256 other)
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return *(long*)(ptr) == *(long*)(other.Bytes) && *(long*)((byte*)ptr + 8) == *(long*)(other.Bytes + 8);
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            var buffer = BufferPool.StaticBuffer.Memory;
+            var len = 0;
+            var span = buffer.Span;
+            var ptr = (byte*)Unsafe.AsPointer(ref this);
+            for (int i = 0; i < Size; i++)
+            {
+                var b = *(ptr + i);
+                if (b == 0)
+                {
+                    break;
+                }
+                span[i] = b;
+                len = i + 1;
+            }
+
+            // TODO use CoreFxLab new encoding features
+            if (MemoryMarshal.TryGetArray((ReadOnlyMemory<byte>)buffer, out var segment))
+            {
+                // ReSharper disable once AssignNullToNotNullAttribute
+                return Encoding.UTF8.GetString(segment.Array, segment.Offset, len);
+            }
+            else
+            {
+                ThrowApplicationException();
+                return String.Empty;
+            }
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Symbol256 && Equals((Symbol256)obj);
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode()
+        {
+            var ptr = Unsafe.AsPointer(ref this);
+            return *(int*)ptr;
+        }
+
+        /// <summary>
+        /// Equals operator.
+        /// </summary>
+        public static bool operator ==(Symbol256 x, Symbol256 y)
+        {
+            return x.Equals(y);
+        }
+
+        /// <summary>
+        /// Not equals operator.
+        /// </summary>
+        public static bool operator !=(Symbol256 x, Symbol256 y)
         {
             return !x.Equals(y);
         }
