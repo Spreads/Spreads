b7594ed73f24938dbe6337d05cc9848a458185a9
buybackoff <vbaybekov@gmail.com>
2018-11-20 00:19:52 +0300

    Copy TaskScheduler for custom thread pool.

--- src/Spreads.Core/Threading/SpreadsThreadPool.cs
+++ src/Spreads.Core/Threading/SpreadsThreadPool.cs
@@ -762,6 +762,8 @@ namespace Spreads.Threading
         // Without accessing this namespace and class it is not created
         private static SpreadsThreadPool _default;
 
+        private static TaskScheduler _scheduler;
+
         public static SpreadsThreadPool Default
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -772,6 +774,31 @@ namespace Spreads.Threading
                     InitDefault();
                 }
                 return _default;
+            }
+        }
+
+        public TaskScheduler Scheduler
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                if (_scheduler == null)
+                {
+                    CreateScheduler();
+                }
+                return _scheduler;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void CreateScheduler()
+        {
+            lock (this)
+            {
+                if (_scheduler == null)
+                {
+                    _scheduler = new SpreadsThreadPoolTaskScheduler(this);
+                }
             }
         }
 
@@ -978,6 +1005,146 @@ namespace Spreads.Threading
         }
 
         #endregion Pool worker implementation
+    }
+
+    /// <summary>
+    /// TaskScheduler for working with a <see cref="SpreadsThreadPool"/> instance
+    /// </summary>
+    internal class SpreadsThreadPoolTaskScheduler : TaskScheduler
+    {
+        // Indicates whether the current thread is processing work items.
+        [ThreadStatic]
+        private static bool _currentThreadIsRunningTasks;
+
+        /// <summary>
+        /// Number of tasks currently running
+        /// </summary>
+        private volatile int _parallelWorkers = 0;
+
+        private readonly LinkedList<Task> _tasks = new LinkedList<Task>();
+
+        private readonly SpreadsThreadPool _pool;
+
+        public SpreadsThreadPoolTaskScheduler(SpreadsThreadPool pool)
+        {
+            _pool = pool;
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            lock (_tasks)
+            {
+                _tasks.AddLast(task);
+            }
+
+            EnsureWorkerRequested();
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            //current thread isn't running any tasks, can't execute inline
+            if (!_currentThreadIsRunningTasks) return false;
+
+            //remove the task from the queue if it was previously added
+            if (taskWasPreviouslyQueued)
+                if (TryDequeue(task))
+                    return TryExecuteTask(task);
+                else
+                    return false;
+            return TryExecuteTask(task);
+        }
+
+        protected override bool TryDequeue(Task task)
+        {
+            lock (_tasks) return _tasks.Remove(task);
+        }
+
+        /// <summary>
+        /// Level of concurrency is directly equal to the number of threads
+        /// in the <see cref="SpreadsThreadPool"/>.
+        /// </summary>
+        public override int MaximumConcurrencyLevel => _pool.Settings.NumThreads;
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            var lockTaken = false;
+            try
+            {
+                Monitor.TryEnter(_tasks, ref lockTaken);
+
+                //should this be immutable?
+                if (lockTaken) return _tasks;
+                else throw new NotSupportedException();
+            }
+            finally
+            {
+                if (lockTaken) Monitor.Exit(_tasks);
+            }
+        }
+
+        private void EnsureWorkerRequested()
+        {
+            var count = _parallelWorkers;
+            while (count < _pool.Settings.NumThreads)
+            {
+                var prev = Interlocked.CompareExchange(ref _parallelWorkers, count + 1, count);
+                if (prev == count)
+                {
+                    RequestWorker();
+                    break;
+                }
+                count = prev;
+            }
+        }
+
+        private void ReleaseWorker()
+        {
+            var count = _parallelWorkers;
+            while (count > 0)
+            {
+                var prev = Interlocked.CompareExchange(ref _parallelWorkers, count - 1, count);
+                if (prev == count)
+                {
+                    break;
+                }
+                count = prev;
+            }
+        }
+
+        private void RequestWorker()
+        {
+            _pool.UnsafeQueueCompletableItem((o) =>
+            {
+                // this thread is now available for inlining
+                _currentThreadIsRunningTasks = true;
+                try
+                {
+                    // Process all available items in the queue.
+                    while (true)
+                    {
+                        Task item;
+                        lock (_tasks)
+                        {
+                            // done processing
+                            if (_tasks.Count == 0)
+                            {
+                                ReleaseWorker();
+                                break;
+                            }
+
+                            // Get the next item from the queue
+                            item = _tasks.First.Value;
+                            _tasks.RemoveFirst();
+                        }
+
+                        // Execute the task we pulled out of the queue
+                        TryExecuteTask(item);
+                    }
+                }
+                // We're done processing items on the current thread
+                finally { _currentThreadIsRunningTasks = false; }
+            }, null, true);
+        }
     }
 
     #region UnfairSemaphore implementation
