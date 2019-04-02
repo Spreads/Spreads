e328f9eaa1c611df64d1c9af66dc7acb785401b7
buybackoff <vbaybekov@gmail.com>
2019-04-03 00:47:26 +0300

    Fix build script so that auto builds have the same version.

--- nuget/local_build_debug.bat
+++ nuget/local_build_debug.bat
@@ -1,12 +1,21 @@
 @echo off
 
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "timestamp=%HH%%Min%%Sec%"
+
+set "build=build%timestamp%"
+echo build: "%build%"
+
 dotnet restore ..\src\Spreads.Core\Spreads.Core.csproj
-dotnet pack ..\src\Spreads.Core\Spreads.Core.csproj -c Debug -o C:\transient\LocalNuget -p:AutoSuffix=True
+dotnet pack ..\src\Spreads.Core\Spreads.Core.csproj -c Debug -o C:\transient\LocalNuget  --version-suffix "%build%" -p:AutoSuffix=True
 
 dotnet restore ..\src\Spreads.Collections\Spreads.Collections.fsproj
-dotnet pack ..\src\Spreads.Collections\Spreads.Collections.fsproj -c Debug -o C:\transient\LocalNuget -p:AutoSuffix=True
+dotnet pack ..\src\Spreads.Collections\Spreads.Collections.fsproj -c Debug -o C:\transient\LocalNuget  --version-suffix "%build%" -p:AutoSuffix=True
 
 dotnet restore ..\src\Spreads\Spreads.csproj
-dotnet pack ..\src\Spreads\Spreads.csproj -c Debug -o C:\transient\LocalNuget -p:AutoSuffix=True
+dotnet pack ..\src\Spreads\Spreads.csproj -c Debug -o C:\transient\LocalNuget --version-suffix "%build%" -p:AutoSuffix=True
 
 pause
