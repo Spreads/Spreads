b2f2368b9c970a032e105f6f002dd1ee78aa1285
Victor Baybekov <vbaybekov@gmail.com>
2021-07-19 23:07:40 +0200

    Fix publish script

--- nuget/push_artifacts.cmd
+++ nuget/push_artifacts.cmd
@@ -7,7 +7,7 @@ setlocal
 SET /P AREYOUSURE=Push to NuGet.org? (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
-@for %%f in (..\artifacts\*.nupkg) do @\tools\nuget\NuGet.exe push %%f -source https://www.nuget.org/api/v2/package
+@for %%f in (..\artifacts\*.nupkg) do dotnet nuget push %%f --source https://www.nuget.org/api/v2/package
 xcopy /s ..\artifacts \transient\LocalNuget
 
 :END
